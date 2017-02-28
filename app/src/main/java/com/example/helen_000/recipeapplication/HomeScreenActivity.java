e793018f1b1d7f7fcaf3a445d0a0db02ca2fe182
Helena Olafsdottir <helenaol@hotmail.com>
2017-02-28 18:27:32 +0000

    fullt af allskonar

--- app/src/main/java/com/example/helen_000/recipeapplication/HomeScreenActivity.java
+++ app/src/main/java/com/example/helen_000/recipeapplication/HomeScreenActivity.java
@@ -2,12 +2,49 @@ package com.example.helen_000.recipeapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import android.content.Intent;
+import android.view.View.OnClickListener;
 
 public class HomeScreenActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+
+
+
     }
+   // Button mHomeScreenRecipeGroupsButton = (Button)findViewById(R.id.mHomeScreenRecipeGroupsButton);
+
+    public void mHomeScreenRecipeGroupsButtonOnClick (View v){
+        Intent intent = new Intent(HomeScreenActivity.this, RecipeGroupActivity.class);
+        startActivity(intent);
+    }
+    public void mHomeScreenSearchButtonOnClick (View v){
+        Intent intent = new Intent(HomeScreenActivity.this, RecipeGroupActivity.class);
+        startActivity(intent);
+    }
+    public void mHomeScreenLoginButtonOnClick (View v){
+        Intent intent = new Intent(HomeScreenActivity.this, RecipeGroupActivity.class);
+        startActivity(intent);
+    }
+    public void mHomeScreenSignupButtonOnClick (View v){
+        Intent intent = new Intent(HomeScreenActivity.this, RecipeGroupActivity.class);
+        startActivity(intent);
+    }
+/*
+    mHomeScreenRecipeGroupsButton.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            startActivity(new Intent(HomeScreenActivity.this, RecipeGroupActivity.class));
+            finish();
+        }
+    });
+*/
 }
